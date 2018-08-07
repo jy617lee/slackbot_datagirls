44f6ab1816b3b1b4351b7e935e57acad02024dfd
최 지영 <jiyoung031@gmail.com>
2018-08-07 12:15:02 +0900

    지난 과오를 반성하고 코드를 복원함.

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
-print('Hello, World!')
+from rtmbot import RtmBot
+from rtmbot.core import Plugin
+import secret
+
+
+class StartReview(Plugin):
+    def process_message(self, data):
+        print(data)
+config = {
+    "SLACK_TOKEN": secret.SLACK_TOKEN,
+    "ACTIVE_PLUGINS": []
+}
+bot = RtmBot(config)
+bot.start()
+
+def process_message(self, data):
+    if "안녕" in data["text"]:
+        self.outputs.append([data["channel"], "오늘 하루도 수고많았어. 오늘은 어땠어?"])
+
+print('화이팅 지윤님')
