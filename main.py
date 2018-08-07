e91b9363adc03b2ebda788db7e30c9854eb4c689
최 지영 <jiyoung031@gmail.com>
2018-08-07 12:03:35 +0900

    쉼표 추가

--- main.py
+++ main.py
@@ -1,18 +1,1 @@
-from rtmbot import RtmBot
-from rtmbot.core import Plugin
-import secret
-
-
-class StartReview(Plugin):
-    def process_message(self, data):
-        print(data)
-config = {
-    "SLACK_TOKEN": secret.SLACK_TOKEN,
-    "ACTIVE_PLUGINS": []
-}
-bot = RtmBot(config)
-bot.start()
-
-def process_message(self, data):
-    if "안녕" in data["text"]:
-        self.outputs.append([data["channel"], "오늘 하루도 수고많았어. 오늘은 어땠어?"])
+print('Hello, World!')
